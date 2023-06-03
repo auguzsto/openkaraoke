0043f71c03ba1b6424edec6b2c57746caf424b7b
Augusto <auguzsto@gmail.com>
2023-06-03 15:01:42 -0300

    fix init dafault path

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:dart_vlc/dart_vlc.dart';
 import 'package:flutter/material.dart';
 import 'package:openkaraoke/src/app_widget.dart';
+import 'package:openkaraoke/src/constants/labels.dart';
+import 'package:openkaraoke/src/view/home/home_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  config.set(Labels.pathDefault);
   DartVLC.initialize();
   runApp(const AppWidget());
 }
--- lib/src/constants/labels.dart
+++ lib/src/constants/labels.dart
@@ -1,9 +1,11 @@
 const String _enterCode = "Digite um código";
 const String _yourNote = "Sua nota";
 const String _textButtonSaveConfig = "Salvar";
+const String _pathDefault = "C:\\musicas\\";
 
 abstract class Labels {
   static String get enterCode => _enterCode;
   static String get yourNote => _yourNote;
   static String get textButtonSaveConfig => _textButtonSaveConfig;
+  static String get pathDefault => _pathDefault;
 }
